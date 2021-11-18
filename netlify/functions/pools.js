038688f519fa525a928f82a00446293d66f7ee88
Travis Horn <travis@travishorn.com>
2021-11-18 10:38:26 -0600

    Using smashpeek api

--- netlify/functions/pools.js
+++ netlify/functions/pools.js
@@ -1,28 +1,16 @@
 const axios = require("axios");
 
 exports.handler = async function (event) {
-  const tickers = event.queryStringParameters.tickers.split(",");
+  const { data: res } = await axios.get("https://smashpeek.com/ppservices/pooldetails/getsundaepools");
 
-  /*
-   * Big problem: This API endpoint doesn't return all pools. 14 of the 30
-   * SundaeSwap pools are missing.
-   */
-  const { data: apRes } = await axios.get("https://a.adapools.org/yoroi-api/");
-
-  const poolData = [];
-
-  Object.entries(apRes.pools).forEach(([index, pool]) => {
-    if (tickers.includes(pool.db_ticker)) {
-      poolData.push({
-        ticker: pool.db_ticker,
-        roa: pool.roa,
-        saturation: pool.saturation,
-        totalStake: pool.total_stake,
-        bpe: pool.blocks_epoch,
-        margin: pool.tax_ratio,
-        pledge: pool.pledge,
-      });
-    }
+  const poolData = res.pools.map((pool) => {
+    return {
+      ticker: pool.ticker,
+      saturation: pool.pct_saturated,
+      stake: pool.live_stake,
+      margin: pool.margin_pct,
+      pledge: pool.pledge.replace(/,/g, ""),
+    };
   });
 
   const poolDataAlpha = poolData.sort((a, b) => {
@@ -35,26 +23,26 @@ exports.handler = async function (event) {
     return curr.ticker.length > prev ? curr.ticker.length : prev;
   }, 4);
 
-  console.log(tickLen);
-
-  const header = `| ${"Pool".padEnd(tickLen)} | ROA   | Sat | Stake  | BPE | Margin | Pledge |
-|${"-".padEnd(tickLen + 2, "-")}|-------|-----|--------|-----|--------|--------|
+  const header = `\`\`\`
+| ${"Pool".padEnd(tickLen)} |  Sat | Stake | Margin | Pledge |
+|${"-".padEnd(tickLen + 2, "-")}|------|-------|--------|--------|
 `;
   
   const tableData = poolDataAlpha.map((pool) => {
     const ticker = pool.ticker.padEnd(tickLen);
-    const roa = `${(+pool.roa).toFixed(2)}%`;
-    const saturation = `${(+pool.saturation * 100).toFixed()}%`;
-    const stake = `${(+pool.totalStake / 1000000000000).toFixed(2)}M`;
-    const bpe = pool.bpe.padStart(3);
-    const margin = `${(+pool.margin * 100).toFixed(2)}%`;
-    const pledge = `${(+pool.pledge / 1000000000000).toFixed(2)}M`;
+    const saturation = `${(+pool.saturation).toFixed().padStart(3)}%`;
+    const stake = `${(pool.stake / 1000000).toFixed(1)}M`.padStart(5);
+    const margin = `${pool.margin}%`.padStart(6);
+    const pledge = `${(+pool.pledge / 1000000).toFixed(2)}M`;
     
-    return `| ${ticker} | ${roa} | ${saturation} | ${stake} | ${bpe} |  ${margin} |  ${pledge} |`;
+    return `| ${ticker} | ${saturation} | ${stake} | ${margin} |  ${pledge} |`;
   });
+
+  const footer = `
+\`\`\``;
 
   return {
     statusCode: 200,
-    body: `${header}${tableData.join("\r\n")}`,
+    body: `${header}${tableData.join("\r\n")}${footer}`,
   };
 };
